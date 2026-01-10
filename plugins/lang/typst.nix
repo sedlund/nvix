aad9eb0d8ddb7ad0c7f1fc4a31eb5da36158d71f
Scott Edlund <sedlund@users.noreply.github.com>
2026-01-10 20:54:44 +0700

    style(nixfmt): format

--- plugins/lang/typst.nix
+++ plugins/lang/typst.nix
@@ -1,11 +1,11 @@
-{pkgs, lib, ...}:
+{ pkgs, lib, ... }:
 {
   plugins.lsp.servers.tinymist = {
     enable = true;
   };
 
   plugins.conform-nvim.settings = {
-    formatters_by_ft.typst = ["typstyle"];
+    formatters_by_ft.typst = [ "typstyle" ];
     formatters.typstyle.command = lib.getExe pkgs.typstyle;
   };
 
